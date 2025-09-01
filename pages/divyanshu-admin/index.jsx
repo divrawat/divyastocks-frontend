aa7e126dbca6bb2329d92f67c96b5839e0425d3a
divrawat2 <divrawat2001second@gmail.com>
2025-09-01 23:17:35 +0530

    rge0g5

--- pages/divyanshu-admin/index.jsx
+++ pages/divyanshu-admin/index.jsx
@@ -71,14 +71,14 @@ const Admin = () => {
                 <div className="relative py-3 sm:w-96 mx-auto text-center">
                     <span className="text-2xl font-light ">Admin Login to your account</span>
                     <div className="mt-4 bg-white shadow-md rounded-lg text-left">
-                        <div className="h-2 bg-lime-400 rounded-t-md"></div>
+                        <div className="h-2 bg-red-400 rounded-t-md"></div>
                         <div className="px-8 py-6 ">
                             <label className="block font-semibold"> Email </label>
                             <input value={email} onChange={handleChange('email')} name="email" type="text" placeholder="Email" className="border w-full h-5 px-3 py-5 mt-2 hover:outline-none focus:outline-none focus:ring-indigo-500 focus:ring-1 rounded-md" />
                             <label className="block mt-3 font-semibold"> Password </label>
                             <input value={password} onChange={handleChange('password')} name="password" type="password" placeholder="Password" className="border w-full h-5 px-3 py-5 mt-2 hover:outline-none focus:outline-none focus:ring-indigo-500 focus:ring-1 rounded-md" />
                             <div className="flex justify-between items-baseline">
-                                <button type="submit" className="mt-4  bg-lime-500 text-white py-2 px-6 rounded-md hover:bg-lime-600 ">Login</button>
+                                <button type="submit" className="mt-4  bg-red-500 text-white py-2 px-6 rounded-md hover:bg-red-600 ">Login</button>
                                 {/* <a href="#" className="text-sm hover:underline">Forgot password?</a> */}
                             </div>
                         </div>
