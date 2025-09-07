d696cf8c8c7d522f25a26348c2cce1c637040940
divrawat2 <divrawat2001second@gmail.com>
2025-09-07 17:24:08 +0530

    rg0eg5

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 
-export const DOMAIN = "http://localhost:3000";
-export const BACKEND = "http://localhost:8000"
+// export const DOMAIN = "http://localhost:3000";
+// export const BACKEND = "http://localhost:8000"
 
-// export const DOMAIN = "https://divyastocks.info";
-// export const BACKEND = "https://divyastocks-backend.vercel.app"
+export const DOMAIN = "https://divyastocks.info";
+export const BACKEND = "https://divyastocks-backend.vercel.app"
 
 export const APP_DESCRIPTION = 'DivyaStocks.info एक भरोसेमंद और ताज़ा समाचार प्रदान करने वाला हिंदी न्यूज़ पोर्टल है, जहाँ आपको मिलती है आईपीओ (IPO) से जुड़ी ताज़ा जानकारियाँ, स्टॉक मार्केट की बड़ी खबरें, बाजार के उतार-चढ़ाव की विश्लेषणात्मक रिपोर्ट और क्रिप्टोकरेंसी से जुड़ी महत्वपूर्ण अपडेट। हमारा उद्देश्य है आपको फाइनेंस और निवेश से जुड़ी हर अहम खबर सबसे पहले और सरल भाषा में पहुँचाना। चाहे आप नए निवेशक हों या अनुभवी ट्रेडर, DivyaStocks.info है आपका भरोसेमंद साथी।';
 
