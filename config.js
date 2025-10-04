93d2cf3b26e0e7f4f1c0198f92f068517e50ae0a
divrawat2 <divrawat2001second@gmail.com>
2025-10-04 11:56:48 +0530

    werf0erg5

--- config.js
+++ config.js
@@ -7,11 +7,11 @@ export const BACKEND = "https://divyastocks-backend.vercel.app"
 
 export const APP_DESCRIPTION = 'Divyastocks.online एक समर्पित हिंदी वित्तीय समाचार प्लेटफ़ॉर्म है जो आपको आईपीओ, शेयर बाज़ार की गतिविधियों, विशेषज्ञ बाज़ार की जानकारी और क्रिप्टोकरेंसी की दुनिया में प्रमुख बदलावों पर रीयल-टाइम अपडेट प्रदान करता है। हम जटिल वित्तीय समाचारों को सरल बनाते हैं, जिससे वे नए निवेशकों से लेकर अनुभवी बाज़ार खिलाड़ियों तक, सभी के लिए सुलभ और समझने योग्य बन जाते हैं। दिव्यस्टॉक्स.ऑनलाइन पर, हम केवल समाचार ही नहीं बताते, बल्कि आपको उन्हें समझने में भी मदद करते हैं, ताकि आप बेहतर निवेश कर सकें और सूचित रह सकें।';
 
-export const APP_TITLE = 'DivyaStocks: शेयर बाजार, IPO और क्रिप्टोकरेंसी समाचार'
+export const APP_TITLE = 'DivyaStocks: IPO, शेयर बाजार और क्रिप्टोकरेंसी समाचार'
 
 // Navbar
 export const logo = <img src="/logo.jpg" alt="Logo" className="h-[50px] w-[50px] mr-5 md:ml-0 ml-4 md:my-0 my-1 rounded-full" />
-export const logolink = `${DOMAIN}/logo.jpg`
+export const logolink = `${DOMAIN}/logo.webp`
 export const APP_NAME = 'DivyaStocks';
 export const NavbarName = "DivyaStocks";
 
